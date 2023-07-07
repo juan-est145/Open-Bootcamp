4810abf515c5575162b10883c7a91e59b95d9215
Juan José Estrella Ramírez <128648875+juan-est145@users.noreply.github.com>
2023-07-07 22:24:52 +0200

    Add files via upload

---
+++ Octavo_ejercicio_de_openbootcamp/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Introduzca un número del 1 al 10");
+int multiplicando = Convert.ToInt32(Console.ReadLine());
+int multiplicador = 1;
+while (multiplicador <= 10)
+{
+    Console.WriteLine($"{multiplicando} x {multiplicador} = {multiplicando*multiplicador}");
+    multiplicador++; 
+}
