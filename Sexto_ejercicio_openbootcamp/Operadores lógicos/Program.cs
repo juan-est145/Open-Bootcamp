b582129c9669ee94ee52601462429328204208c0
Juan José Estrella Ramírez <128648875+juan-est145@users.noreply.github.com>
2023-07-07 14:24:28 +0200

    Add files via upload

---
+++ Sexto_ejercicio_openbootcamp/Operadores lógicos/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operadores_lógicos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero = 19;
+            char caracter= 'w';
+
+            Console.WriteLine($"¿Es {numero} mayor o igual a 18? {numero >= 18}");
+            Console.WriteLine($"¿Es el caracter igual a 'a'? {caracter == 'a'}");
+            Console.WriteLine($"¿Se cumplen las dos condiciones anteriores a la vez? {numero >= 18 && caracter == 'a'}");
+            Console.WriteLine($"¿Se cumple al menos una de las dos condiciones anteriores? {numero >= 18 || caracter == 'a'}");
+            Console.ReadKey();
+        }
+    }
+}
