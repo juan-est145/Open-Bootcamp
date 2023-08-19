f4734c3b96ac650575119518f23173d02ccee15c
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2023-08-20 00:08:01 +0200

    Fin de la sesión 5.

--- api-restful-backend/Controllers/AccountsController.cs
+++ api-restful-backend/Controllers/AccountsController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using api_restful_backend.Models.DataModels;
+using api_restful_backend.Helpers;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 
 namespace api_restful_backend.Controllers
 {
@@ -32,5 +36,41 @@ namespace api_restful_backend.Controllers
                 Password = "Prueba"
             }
         };
+        [HttpPost]
+        public IActionResult GetToken(UserLogins userLogin)
+        {
+            try
+            {
+                var Token = new UserTokens();
+                var Valid = Logins.Any(user => user.Name.Equals(userLogin.userName, StringComparison.OrdinalIgnoreCase));
+
+                if (Valid)
+                {
+                    var user = Logins.FirstOrDefault(user => user.Name.Equals(userLogin.userName, StringComparison.OrdinalIgnoreCase));
+                    Token = JWTHelpers.GenTokenKey(new UserTokens()
+                    {
+                        UserName = user.Name,
+                        EmailId = user.EmailAddress,
+                        Id = user.Id,
+                        GuidId = Guid.NewGuid()
+                    }, _jwtSettings);  
+                }
+                else
+                {
+                    return BadRequest("Wrong Password");
+                }
+                return Ok(Token);
+            }
+            catch (Exception ex) 
+            {
+                throw new Exception("GetToken Error", ex);
+            }
+        }
+        [HttpGet]
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme, Roles = "Administrator")]
+        public IActionResult GetUserList()
+        {
+            return Ok(Logins);
+        }
     }
 }
--- api-restful-backend/Models/DataModels/UserTokens.cs
+++ api-restful-backend/Models/DataModels/UserTokens.cs
@@ -2,7 +2,7 @@
 {
     public class UserTokens
     {
-        public Guid Id { get; set; }
+        public int Id { get; set; }
         public string Token { get; set; }
         public string UserName { get; set; }
         public TimeSpan Validity { get; set; }
