a81bc23b8cb36435148493412d12d6441bd511e2
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2023-08-22 19:24:51 +0200

    Fin de la sesión 7.

---
+++ Internacionalización y versiones/Entities/SharedResource.cs
@@ -0,0 +1,6 @@
+﻿namespace Internacionalización_y_versiones.Entities
+{
+    public class SharedResource
+    {
+    }
+}
--- Internacionalización y versiones/Program.cs
+++ Internacionalización y versiones/Program.cs
@@ -1,6 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+//1. Localization
+builder.Services.AddLocalization(options => options.ResourcesPath = "Resources");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -8,6 +13,15 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//2. Supported cultures3
+var supportedCultures = new[] { "en-Us", "es-ES","fr-FR" }; //USA's English, Spain's Spanish and France's french.
+var localizationOptions = new RequestLocalizationOptions().SetDefaultCulture(supportedCultures[0])
+    .AddSupportedCultures(supportedCultures) //Add all supported cultures
+    .AddSupportedUICultures(supportedCultures); //add supported cultures to UI;
+
+//3. Add localization to the app
+app.UseRequestLocalization(localizationOptions);
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
