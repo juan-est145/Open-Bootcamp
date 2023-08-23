9d0d7425330098836aaee745e1c673a89fa78706
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2023-08-23 17:12:24 +0200

    Fin de la sesión 8

---
+++ APIVersionControl/Controllers/V2/UsersControllerV2.cs
@@ -0,0 +1,38 @@
+﻿using APIVersionControl.DTO;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace APIVersionControl.Controllers.V2
+{
+    [ApiVersion("2.0")]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private const string ApiTestURl = "https://dummyapi.io/data/v1/user?limit=30";
+        private const string AppID = "64e5ecd624c779b0f7008011";
+        private readonly HttpClient _httpClient;
+
+        public UsersController(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        [MapToApiVersion("2.0")]
+        [HttpGet(Name = "GetUsersData")]
+        public async Task<IActionResult> GetUsersDataAsync()
+        {
+            _httpClient.DefaultRequestHeaders.Clear();
+            _httpClient.DefaultRequestHeaders.Add("app-id", AppID);
+
+            var response = await _httpClient.GetStreamAsync(ApiTestURl);
+
+            var usersData = await JsonSerializer.DeserializeAsync<UsersResponseData>(response);
+
+            var users = usersData?.data;
+
+            return Ok(users);
+        }
+    }
+}
