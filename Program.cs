15eee510549307d4aa6a972124bfb816405c4d3d
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-18 15:11:25 +0100

    Changed folders name

--- Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Introduce tu nombre");
-string nombre = Console.ReadLine();
-Console.WriteLine($"Hola {nombre}, que tengas un buen día");
-Console.WriteLine("Introduce la hora");
-string hora = Console.ReadLine();
-Console.WriteLine($"La hora que has escrito es las {hora}");
