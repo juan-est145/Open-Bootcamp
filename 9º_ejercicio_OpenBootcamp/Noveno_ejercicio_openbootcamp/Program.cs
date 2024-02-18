b244ef496257c1dc987af5841f235150fcbbad03
juan-est145 <128648875+juan-est145@users.noreply.github.com>
2024-02-18 22:20:56 +0100

    Changed 9th name

---
+++ 9º_ejercicio_OpenBootcamp/Noveno_ejercicio_openbootcamp/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Introduzca su nombre");
+string nombre = Console.ReadLine().ToLower();
+Console.WriteLine("Introduzca su email");
+string email = Console.ReadLine();
+Console.WriteLine("Si tiene un cupón, pulse 1, en caso contrario, pulse 0");
+int aplicaDescuento = Convert.ToInt32(Console.ReadLine());
+
+int precio = 20;
+float descuento = 0.25f; 
+
+if  (aplicaDescuento == 1)
+{
+    Console.WriteLine($"Muy buenas {nombre}, tiene un descuento del {descuento * 100}%, por lo que el precio del producto se queda en {(precio * (descuento * 100) /100)} euros");
+}
+else if (aplicaDescuento == 0)
+{
+    Console.WriteLine($"Muy buenas {nombre}, el precio del producto es {precio} euros");
+}
